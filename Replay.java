226af55a6becf240a0ec06ab0cde9a7b74921e8d
Harishankar-S <100881953+Harishankar-S@users.noreply.github.com>
2022-03-10 21:03:26 -0600

    Update Replay.java

--- Replay.java
+++ Replay.java
@@ -5,40 +5,9 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Replay
 {
-//	public static void main(String[] args)
-//	{
-//		final String FILEPATH = "src\\tictactoe\\Data\\";
-//		
-//		try
-//		{
-//			viewReplay(FILEPATH, 5, 2500);
-//			System.out.println("And that's the game!");
-//			
-//			saveReplay("src\\tictactoe\\Data\\replay-0.txt", new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8});
-//		}
-//		catch (IOException e)
-//		{
-//			System.out.println("An IO Exception occured.");
-//			System.exit(0);
-//		}
-//		catch (InterruptedException e)
-//		{
-//			System.out.println("The thread was interrupted.");
-//			System.exit(1);
-//		}
-//		catch (Exception e)
-//		{
-//			System.out.println("An unknown error occurred.");
-//			System.exit(2);
-//		}
-//	}
-	
-	
 	public static void viewReplay(String filepath, int replayNumber, int speed) throws IOException, InterruptedException
 	{
 		String filename = "replay-" + replayNumber + ".txt";
@@ -51,8 +20,13 @@ public class Replay
 		{
 			int coordinates = Integer.parseInt(reader.readLine());
 			
-			if (i % 2 == 0)		{moves[coordinates] = "X";}
-			else				{moves[coordinates] = "O";}
+			if (coordinates == 0)
+			{
+				System.out.println("Game has ended.");
+				System.exit(0);
+			}
+			else if (i % 2 == 0){moves[coordinates-1] = "X";}
+			else				{moves[coordinates-1] = "O";}
 			
 			System.out.println
 			(
